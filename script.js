19670302be1922084455b881b1662bfb54f1e06c
John Olivares <olivaresvjohn@gmail.com>
2018-07-25 22:32:27 -0400

    Separando el js del html

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+			//ponemos nuestro código JavaScript que utiliza jQuery
+
+			configurarInicio();
+
+			$('.myicono').click(function(e){
+				//console.dir(e);
+				//alert('Prueba');
+				$('#navbar ul').toggleClass('lista-sm');
+			})
+
+			function configurarInicio(){
+				var urlPath = window.location.pathname;
+				console.log(urlPath);
+				$('nav a').each(function(){
+					var href = $(this).attr('href');
+					console.log(href);
+					var indice = urlPath.length - href.length;
+					console.log(urlPath.substring(indice));
+					if(urlPath.substring(indice) === href){
+						console.log('match');
+						$(this).closest('li').addClass('active');
+					}
+				})
+			}
+		})
